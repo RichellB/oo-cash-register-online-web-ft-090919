4f9870384708ce5d171524f26bd4c9e16cf3bd38
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-22 20:16:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister 
+  def initialize
+    @total = 0
+   # @employee_discount = employee_discount
+  end
+  
+  def total 
+    self.total
+  end 
+  
+  def add_item(title, price, quantity = 0)
+    previous_total = @@total
+    @@total += 1 
+    previous_total
+  end 
+end
