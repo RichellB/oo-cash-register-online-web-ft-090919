09fe5233cce51c1cba8339000ea71aa208952467
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-22 20:42:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity = 1)
-    self.total = price * quantity 
+    self.total += price * quantity 
     @items << title
   end 
 end
